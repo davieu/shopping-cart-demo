723dd53d6ddc0f6ea16471c0cfbe5f91c9902210
davieu <davisumana@gmail.com>
2019-07-07 02:50:59 -0400

    function formatPrices for displaying pricing functions correctly

--- vue-shop/src/components/Products.vue
+++ vue-shop/src/components/Products.vue
@@ -4,30 +4,49 @@
       
     <b-container>
             <b-row>
-              <b-col cols="4" v-for="(product) in allProducts" :key="product._id">
+              <b-col cols="6" md="4" v-for="(product) in allProducts" :key="product._id">
                 <b-card-group deck class="mb-3">
-                <b-card border-variant="light" :header="product.productName" class="text-center" >
+                <b-card :header="`${product.productName}`" class="text-center cards">
+                  <p class="price">Price: {{ formatPrices(product.price) }}</p>
+                  <hr>
                   <b-card-text>{{product.description}}</b-card-text>
-                  <b-button>Add to Cart!</b-button>
+                  <b-button variant="success" class="add-cart-btn">Add to Cart!</b-button>
                 </b-card>
                 </b-card-group>
               </b-col>
             </b-row>
     </b-container>
-     
   </div>
 </template>
 
 <script>
 // { "_id": "5d1ff799f374df8498415fc0", "productName": "orange", "price": 0.25, "description": "A juicy citrus that will enrich you with vitamin C!", "__v": 0 } ]
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters, mapState} from 'vuex';
 export default {
   name: 'Products',
   methods: {
-    ...mapActions(['fetchProducts'])
+    ...mapActions(['fetchProducts']),
+    formatPrices(prices) {
+      // I used a try catch blocks because a bug keeps appearing when creating a new product. The err happens when a new product is added and the item has not loaded in but the function runs before the created product is loaded in. The functionality works but I stopped the err from initiating.
+      try {
+        // this formats the pricings and depending on where the decimal is. I converted number to string.
+        let pricesToString = prices.toString()
+        let returnPrice
+        let indexOfDecimal = pricesToString.indexOf('.');
+        if (indexOfDecimal == -1) {
+          returnPrice = pricesToString + '.00' //1 >>> 1.00
+        } else if (pricesToString[pricesToString.length - 2] == '.') {
+          returnPrice = pricesToString + '0' //1.1 >>> 1.10
+        } else {
+          returnPrice = pricesToString // for prices that are already correctly formatted
+        }
+        return '$ ' + returnPrice //just adds a $ to the prices
+      } catch(err) {}
+    }
   },
   computed: {
-   ...mapGetters(['allProducts'])
+    ...mapGetters(['allProducts']),
+    ...mapState(['products'])
   },
   created() {
     this.fetchProducts();
@@ -41,6 +60,28 @@ body {
   font-family: "Franklin Gothic Medium", "Ariel Narrow", Arial, sans-serif;
   line-height: 1.6;
   background: #e8f7f0
+}
+
+.price {
+  text-align: right;
+}
+
+.card {
+  border: 2px solid #41b883;
+  border-radius: 8px;
+  height: 275px;
+}
+
+.card-header {
+  background: #41b883;
+  border-radius: 10px;
+  font-weight: bold;
+}
+
+.add-cart-btn {
+  right:20px;
+  bottom: 15px;
+  position: absolute;
 }
 
 h3 {
--- vue-shop/src/store/mutations.js
+++ vue-shop/src/store/mutations.js
@@ -1,5 +1,12 @@
 export default {
-  setProducts: (state, products) => (state.products = products),
+  setProducts: (state, products) => {
+    products.forEach(product => {
+      // quick name shortener
+      let prodName = product.productName
+      // uppercases the first letter of the product names in the state
+      product.productName = prodName[0].toUpperCase() + prodName.substring(1, prodName.length)});   
+      state.products = products
+  },
   newProduct: (state, {addedProduct}) => {
     setTimeout(() => state.requestStatus = null, 1500)
     state.requestStatus = true;
