0028b53b14f0dcb74f67256cd1f910b192eb3ca7
davieu <davisumana@gmail.com>
2019-07-08 17:13:33 -0400

    heroku test

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public/'))
   // Handle SPA single page application
   //for any routes /.*/ send to built file public/index.html
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+  app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
 const PORT = process.env.PORT || 5001;
