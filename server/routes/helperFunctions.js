0ad2b7ae050068905b7355fc8e6203bec0c62122
davieu <davisumana@gmail.com>
2020-06-12 22:48:11 -0400

    helper not working

--- server/routes/helperFunctions.js
+++ server/routes/helperFunctions.js
@@ -1,15 +1,15 @@
 // test
-const addedProduct = new Product(req.body);
-try {
-  const result = await addedProduct.save();
-  console.log(result);
-  return res.send(result);
-} catch (err) {
-  let productNameNotUnique = null;
-  let allProducts = await Product.find().exec();
-  //looks through the DB and looks for an identical product. Then flags it.
-  allProducts.forEach((dbProduct) => {
-    if (dbProduct.productName === addedProduct.productName)
-      productNameNotUnique = true;
-  });
-}
+// const addedProduct = new Product(req.body);
+// try {
+//   const result = await addedProduct.save();
+//   console.log(result);
+//   return res.send(result);
+// } catch (err) {
+//   let productNameNotUnique = null;
+//   let allProducts = await Product.find().exec();
+//   //looks through the DB and looks for an identical product. Then flags it.
+//   allProducts.forEach((dbProduct) => {
+//     if (dbProduct.productName === addedProduct.productName)
+//       productNameNotUnique = true;
+//   });
+// }
